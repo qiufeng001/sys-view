3aad51ecdd6f13291113c54bf9409298e79eea73
qiufeng001 <2227052723@qq.com>
2020-03-20 18:35:09 +0800

    登录模块

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -1,24 +1,50 @@
 import * as React from 'react';
 import axios from 'axios';
 import indexUrl from "../../api/portal";
+import cookie from 'react-cookies'
 const url = indexUrl.index;
 
-class Index extends React.Component<Index> {
+interface IProps {
+    ticket: string
+}    
+
+class Index extends React.Component<Index, IProps> {
     constructor(props: Readonly<Index>) {
         super(props);
+        this.state =  { ticket: cookie.load('ticket') }
     }
 
     componentDidMount = () => {
-        axios.post(`${url}?name=333`).then((res)=>{
+        debugger
+        const ticket = this.state.ticket;
+        if(ticket == null || ticket == '' || ticket == undefined) {
+            window.location.href = `${url}`;
+        }
+        axios.post(`${url}`).then(res => {
             debugger
-            console.log("返回的结果为：" + res.data);
-            return res.data;
+        }).catch(err => {
+            debugger
+            const location = window.location;
+            const searh = location.search;
+            // window.location.href = location.origin + location.pathname + searh;
+            if(searh.indexOf('ticket') != -1) {
+                const ticketArr = searh.split('ticket=');
+                const ticket = ticketArr[1];
+                cookie.save('ticket', ticket);
+                this.setState({ticket: ticket})
+            }
+            // window.location.href = location.origin + location.pathname + searh;
         });
+        // axios.post(`${url}?name=333`).then((res)=>{
+      
+        // }).catch(err => {
+
+        // });
     }
 
     render() {
         return (
-            <body onLoad={this.componentDidMount}></body>
+            <div onLoad={this.componentDidMount}></div>
         );
     }
 }
